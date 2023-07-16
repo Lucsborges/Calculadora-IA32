ba393a0807997d977c4ef38298db8f6b68850749
Lucas Sousa Borges <lborges343@gmail.com>
2023-07-16 02:03:30 -0300

    adicionado o começo da função de soma

--- Soma.asm
+++ Soma.asm
@@ -1,38 +1,81 @@
 ;   Calculadora INTEL-32
 ;   Arquivo de funcoes de SOMA
 ;
+%define    numero1_16   [ebp-4]
+%define    numero2_16   [ebp-6]
+
+%define    numero1_32   [ebp-4]
+%define    numero2_32   [ebp-8]
+
+%define    N16bits       0x30
+%define    N32bits       0x31
+
+
 
 section .data
-    msg1        db      "Digite o primeiro numero: ", 0
-    size_msg1   equ     $-msg1
+    msgn1        db      "Digite o primeiro numero: ", 0
+    size_msgn1   equ     $-msgn1
 
-    msg2        db      "Digite o segundo numero: ", 0
-    size_msg2   equ     $-msg2
+    msgn2        db      "Digite o segundo numero: ", 0
+    size_msgn2   equ     $-msgn2
 
-
-section .bss
-
-
+    extern precisao
 
 section .text
-    
+ 
     global  _SOMA
 
+    extern  _MENU
     extern  _print
     extern  _toInt
     extern  _inputStr
+    extern  _inputN
     extern  _inputN16
     extern  _inputN32
+    extern  _SAIR
 
 _SOMA:
+    push    ebp
+    mov     ebp, esp
 
-    push    msg1
-    push    size_msg1
+    ; "Digite o primeiro numero: "
+    push    msgn1
+    push    size_msgn1
+    call    _print
+
+    call _inputN
+
+    ; "Digite o segundo numero: "
+    push    msgn2
+    push    size_msgn2
+    call    _print
+
+    call _inputN
+
+    mov     AL, [precisao]
+    cmp     AL, N16bits
+    je      _SOMA16
+    cmp     AL, N32bits
+    je      _SOMA32
+    jmp    _SAIR
+
+_SOMA16:
+    xor     eax, eax
+    mov     ax, numero1_16
+    add     ax, numero2_16
+    push    eax
+    push    dword 2
     call    _print
 
 
 
-    ; Terminar o programa
-    mov     eax, 1        ; Número da chamada de sistema para sair
-    xor     ebx, ebx      ; Código de retorno (0)
-    int     80h           ; Chamar a interrupção 80h
+
+
+add     esp, 4      ; volta o esp para o valor anterior
+pop     ebp
+jmp     _MENU
+_SOMA32:
+
+add     esp, 8
+pop     ebp
+jmp     _MENU
--- calculadora.asm
+++ calculadora.asm
@@ -2,6 +2,10 @@
 ;   Arquivo principal, onde são chamadas as principais funções do programa
 ;
 
+
+%define    N16bits       0x30
+%define    N32bits       0x31
+
 section .data
 
     msg_bem_vindo       db      "Bem-vindo. Digite seu nome: ", 0
@@ -25,16 +29,19 @@ section .data
 section .bss
     ; As únicas variáveis globais podem ser os ponteiros para os strings de texto das mensagens,
     user_name   resb    256     ;uma variável para receber o nome do usuário,
-    precisao    resd    1       ;uma variável para receber a resposta de precisão
+    user_len    equ     $-user_name
+
+    precisao    resb    2       ;uma variável para receber a resposta de precisão
                                 ; e
-    input       resd    1       ;uma variável para receber a opção do menu 
+    input       resb    2       ;uma variável para receber a opção do menu 
                                 ;TODAS as outras variáveis DEVEM ser LOCAIS NA PILHA.
 
-
+    global precisao
 section .text
     global  _start
     global  _print
     global  _inputStr
+    global  _inputN
     global  _inputN16
     global  _inputN32
     global  _toInt
@@ -48,7 +55,6 @@ section .text
     ;extern  _EXPONENCIACAO
     ;extern  _MOD
 
-
 _start: ; Main program
 
 ; O programa principal deve UNICAMENTE chamar funções, e receber as saídas das funções que podem 
@@ -58,36 +64,39 @@ _start: ; Main program
 ; outras funções.
     
     ; "Bem-vindo. Digite seu nome: "
-    push    msg_bem_vindo              
-    push    size_msg_bem_vindo        
-    call    _print
+    ;push    msg_bem_vindo              
+    ;push    size_msg_bem_vindo        
+    ;call    _print
 
     ; Ler o nome do usuário
-    push    user_name                  
-    push    255                        
-    call    _inputStr
+    ;push    user_name                  
+    ;push    255                        
+    ;call    _inputStr
+    
+
+    
 
     ; "Hola, "
-    push    msg_hola1
-    push    size_hola1
-    call   _print
+    ;push    msg_hola1
+    ;push    size_hola1
+    ;call   _print
 
     ; Escrever o nome do usuário
-    push    user_name
-    push    255
-    call    _print
+    ;push    user_name
+    ;push    user_len
+    ;call    _print
     
     ; ", bem-vindo ao programa de CALC IA-32"
-    push    msg_hola2
-    push    size_hola2
-    call   _print
+    ;push    msg_hola2
+    ;push    size_hola2
+    ;call   _print
 
 
     ; ============= Escolha da precisão ======================
     ; "Vai  trabalhar  com  16  ou  32  bits  (digite  0  para  16,  e  1  para  32):"
-    push    msg_precisao
-    push    size_msg_precisao
-    call   _print
+    ;push    msg_precisao
+    ;push    size_msg_precisao
+    ;call   _print
 
     ; Ler a precisão
     push    input
@@ -95,9 +104,8 @@ _start: ; Main program
     call    _inputStr
 
     ; Converter o valor lido da precisão para inteiro
-    push    precisao
-    push    10
-    call    _toInt
+    ;push    precisao
+    ;call    _toInt
    
 _MENU:
     ; ============= Escolha da operação ======================
@@ -109,22 +117,23 @@ _MENU:
     ; -  5:  EXPONENCIACAO 
     ; -  6:  MOD
     ; -  7:  SAIR
-    push    msg_menu
-    push    size_msg_menu
-    call   _print
+    ;push    msg_menu
+    ;push    size_msg_menu
+    ;call   _print
 
 
     ; Ler a opção
     push    input
-    push    255
+    push    2
     call    _inputStr
 
     ; Converter o valor lido da opção para inteiro
-    ;push    input
-    ;push    10
+    ;push    WORD[input]
     ;call    _toInt
+    
+    mov     AL, [input]
 
-    cmp     WORD [input], 1
+    cmp     AL, 0x31
     je      _SOMA
     cmp     WORD [input], 2
     ;je      _SUBTRACAO
@@ -141,7 +150,6 @@ _MENU:
 
 
 
-
 _SAIR:
     ; Terminar o programa
     mov     eax, 1        ; Número da chamada de sistema para sair
@@ -149,14 +157,15 @@ _SAIR:
     int     80h           ; Chamar a interrupção 80h
 
 
-_toInt:
-    mov     eax, 0
-    mov     edx, 0
-    mov     ecx, [esp+4]
-    mov     ebx, [esp+8]
-    int     80h
-    mov     [ebx], eax
-    ret     8
+_toInt: ; Converte uma string de numero para inteiro e retorna em BL
+    push    ebp
+    mov     ebp, esp
+    mov     ebx, 0
+    mov     BL, [ebp+8]
+    sub     BL, 0x30
+    mov     BYTE [ebp+8], BL
+    pop     ebp
+    ret     2
 
 ; A função principal e funções de entrada e saída de dados devem estar no mesmo arquivo 
 ; CALCULADORA.ASM.
@@ -167,48 +176,49 @@ _toInt:
 _print:
 
     mov     eax, 4          ; Número da chamada de sistema para escrever
-    mov     ebx, 1          ; Descritor de arquivo (1 para a saída padrão - STDOUT)
-    mov     ecx, [esp+8]    ; Colocar o endereço da string em ecx 
-    mov     edx, [esp+4]    ; Colocar o número de bytes a serem escritos em edx
-    int     80h             ; Chamar a interrupção 80h
+    mov     ebx, 1          ; (1 : saída padrão - STDOUT)
+    mov     ecx, [esp+8]    ; Endereço da string em ecx 
+    mov     edx, [esp+4]    ; Número de bytes a serem escritos em edx
+    int     80h             ; Chamar a interrupção 80h 
     ret     8
 
 
 _inputStr:
     mov     eax, 3          ; Número da chamada de sistema para ler
     mov     ebx, 0          ; Descritor de arquivo (0 para a entrada padrão - STDIN)
-    mov     ecx, [esp+8]    ; Colocar o endereço do buffer em ecx
+    mov     ecx, [esp+8]    ; Guardar a string em ecx
     mov     edx, [esp+4]    ; Colocar o número de bytes a serem lidos em edx
     int     80h             ; Chamar a interrupção 80h
+    mov     BYTE [ecx+eax-1], 0  ; retira o enter da string
     ret     8
+
+_inputN:
+    mov     AL, [precisao]
+    cmp     AL, N16bits
+    je      _inputN16
+    cmp     AL, N32bits
+    je      _inputN32
 
 _inputN16:
     mov     eax, 3
     mov     ebx, 0
-    mov     cx, [esp+6]
+    mov     cx, [esp-4]
     mov     dx, 2
     int     80h
-    ret     4
+    sub     esp, 2
+    ret     
 
 _inputN32:
     mov     eax, 3
     mov     ebx, 0
-    mov     ecx, [esp+8]
+    mov     ecx, [esp-4]
     mov     edx, 4
     int     80h
-    ret     8
-
-
-
-
-
-
+    sub     esp, 4
+    ret     
 
 
 ; ================= Coisas para fazer ainda =================
+; Fazer as operações
 
-; Arrumar a linha de Hola, para "Hola, <nome> bem-vindo ao programa de CALC IA-32"
-
-; Fazer a parte de escolha da precisão
-
-; Fazer todas as operações
+; input de numeros
